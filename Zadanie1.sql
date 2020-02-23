1cc24d54dccaaeb4b8f98c64c2c76bd815224b7b
romanzh1 <61348803+romanzh1@users.noreply.github.com>
2020-02-23 18:35:22 +0300

    Update Zadanie1.sql

--- Zadanie1.sql
+++ Zadanie1.sql
@@ -1,1 +1,33 @@
-12у
+--#1.1
+Select st.name, st.surname
+From students st
+Where st.score >= 4 and st.score <= 4.5
+--#1.2
+Select st.name, st.surname
+From students st
+Where not st.score < 4 and not st.score > 4.5
+--#1.3
+Select st.name, st.surname
+From students st
+Where st.score between 4 and 4.5
+--#1.4
+Select st.name, st.surname
+From students st
+Where not st.score between 0 and 4 and not st.score between 4.5 and 5
+--#2
+Select st.name, st.surname
+From students st
+WHERE st.n_group::varchar like '2%'
+--#3
+Select st.n_group, st.name, st.surname
+From students st
+Order by st.n_group, st.name
+--#4
+Select st.n_group, st.name, st.surname
+From students st
+Order by st.n_group, st.name
+--#5
+Select h.name, h.risk
+From hobbies h
+Where h.name = 'Футбол' or h.name = 'Хоккей'
+--%6
