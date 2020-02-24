53b9024ab1992e30d56276822a3d0a37332f2bee
romanzh1 <61348803+romanzh1@users.noreply.github.com>
2020-02-24 16:17:06 +0300

    Update Zadanie2_group_function.sql

--- Zadanie2_group_function.sql
+++ Zadanie2_group_function.sql
@@ -1,4 +1,17 @@
+--#1
 Select st.n_group, count(st.n_group)
 From students st
 Group by st.n_group
 Order by st.n_group desc
+--#2
+Select st.n_group, avg(st.score)
+From students st
+Group by st.n_group
+Order by st.n_group desc
+--#3
+Select count(distinct st.name)
+From students st
+--#4
+Select count(avg(st.n_group))
+From students st
+Where st.ngroup like '20__' or st.ngroup like '22__'
