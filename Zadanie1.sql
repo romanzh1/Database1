3b8731b57fa414ad59517b28b340e0200f6e0a4d
romanzh1 <61348803+romanzh1@users.noreply.github.com>
2020-02-24 15:37:45 +0300

    Update Zadanie1.sql

--- Zadanie1.sql
+++ Zadanie1.sql
@@ -30,7 +30,45 @@ Order by st.n_group, st.name
 Select h.name, h.risk
 From hobbies h
 Where h.name = 'Футбол' or h.name = 'Хоккей'
---6
+--#6
 Select sth.student_id, sth.hobby_id
 From students_hobbies sth
 Where sth.date_start > '2012-02-01' and sth.date_finish < '2019-12-01' and sth.student_id > 10
+--#7
+Select st.name, st. surname, st.score
+From students st
+Where st.score > 4.5
+Order by st.score desc
+--#8.1
+Select st.name, st. surname, st.score
+From students st
+Where st.score > 4.5
+Order by st.score desc
+Limit 5
+--#8.2
+Select st.name, st. surname, st.score
+From students st
+Where st.score > 4.5
+Order by st.score desc
+Offset 2 limit 5
+--#8.3
+Select st.name, st. surname, st.score
+From students st
+Where st.score > 4.5
+Order by st.score
+Offset 8
+--#9
+Select h.name,
+Case
+	When h.risk >= 8 Then 'Очень высокий'
+	When h.risk >= 6 and h.risk < 8 Then 'Высокий'
+	When h.risk >= 4 and h.risk < 6 Then 'Средний'
+	When h.risk >= 2 and h.risk < 4 Then 'Низкий'
+	When h.risk < 2 Then 'Очень низкий'
+End as risk
+From hobbies h
+--#10
+Select h.name
+From hobbies h
+Order by h.risk desc
+Limit 3
