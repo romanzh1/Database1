ea51ac1f6134c7d09f696ca080c78c64199bdac3
romanzh1 <61348803+romanzh1@users.noreply.github.com>
2020-02-28 15:11:38 +0300

    Update Zadanie2_group_function.sql

--- Zadanie2_group_function.sql
+++ Zadanie2_group_function.sql
@@ -4,20 +4,33 @@ From students st
 Group by st.n_group
 Order by st.n_group desc
 --#2
-Select st.n_group, avg(st.score)
+Select st.n_group, max(st.score)
 From students st
 Group by st.n_group
 Order by st.n_group desc
 --#3
 Select count(distinct st.name)
 From students st
---#5-
-Select avg(st.score)
-From students st
---#6 ?
-                 
---#7 ?
-Select count(st.n_group), avg(st.score), st.n_group
-From students st
-Where avg(st.score) > 3.5
-Group by n_group
+--#5
+SELECT st.n_group, Avg(st.score)
+From Students st
+Group by st.n_group
+Order by st.n_group
+--#6
+Select st.n_group, Avg(st.score)
+From Students st
+Group by st.n_group
+Order by avg desc
+Limit 1                 
+--#7
+Select st.n_group, Avg(st.score)
+From Students st
+Group by st.n_group
+Having avg(st.score) <= 4.5
+Order by avg
+--#8
+Select st.n_group, count(st.name), max(st.score),avg(st.score), min(st.score)
+From Students st
+Group by st.n_group
+Order by st.n_group
+
