69f6ee33adbdb2d14c338157a800e4d54b832879
romanzh1 <61348803+romanzh1@users.noreply.github.com>
2020-02-29 17:31:33 +0300

    Update Zadanie3_Multi-table_queries.sql

--- Zadanie3_Multi-table_queries.sql
+++ Zadanie3_Multi-table_queries.sql
@@ -13,3 +13,20 @@ Group by st.id
 Order by min
 Limit 1
 --#3
+Select st.name, st.surname, st.score, avg(st.score), sum(h.risk)
+From students st
+Inner join students_hobbies sth
+On st.id = sth.student_id
+Inner join hobbies h
+On h.id = sth.hobby_id
+Group by st.name, st.surname, st.score
+Having sum(h.risk) > 0.9 and st.score > (select avg(score) from students)
+--#4
+Select st.name, h.name, sth.date_finish, sth.date_start, (sth.date_finish-sth.date_start)/30 as mounth
+From students st
+Inner join students_hobbies sth
+On st.id = sth.student_id
+Inner join hobbies h
+On h.id = sth.hobby_id
+Where not sth.date_finish is null
+Group by st.name, h.name, sth.date_finish, sth.date_start
