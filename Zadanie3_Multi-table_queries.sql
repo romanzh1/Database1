e70904047c229db4ffde5ea2a38e8f0d74d2ec6b
romanzh1 <61348803+romanzh1@users.noreply.github.com>
2020-02-29 16:52:13 +0300

    Update Zadanie3_Multi-table_queries.sql

--- Zadanie3_Multi-table_queries.sql
+++ Zadanie3_Multi-table_queries.sql
@@ -1,4 +1,15 @@
-SELECT st.name, st.surname, h.name
-FROM students st
-INNER JOIN hobbies h
-ON st.id = h.id
+--#1
+Select st.name, st.surname, h.name
+From students st
+Inner join hobbies h
+On st.id = h.id
+--#2
+Select st.*, min(sth.date_start)
+From students st
+Inner join students_hobbies sth
+On st.id = sth.id
+Where sth.date_finish is null
+Group by st.id
+Order by min
+Limit 1
+--#3
