243a239161d66b18bc5b13eee5b4408bfd2f1ef2
romanzh1 <61348803+romanzh1@users.noreply.github.com>
2020-03-07 15:57:03 +0300

    Update Seminar4.sql

--- Seminar4.sql
+++ Seminar4.sql
@@ -1,1 +1,9 @@
-
+--#1 разделить слово функция
+Select substr('слово',1,3)
+--#2 разделить группу по символам
+Select substr(st.n_group::varchar,1,1)
+From students st
+--#3 вывод студентов 2 и 3 курса и их среднего балла
+Select substr(st.n_group::varchar,1,1), avg(st.score)
+From students st
+Group by substr(st.n_group::varchar,1,1)
